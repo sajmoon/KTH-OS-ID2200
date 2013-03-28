6c2fda4ab9b5999a03918b74c4df05294a815eca
Simon Strom <simstr@kth.se>
2013-03-28 20:08:52 +0100

    final for the day. lets go home

--- prepare3.3.4.c
+++ prepare3.3.4.c
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 /* Required by for routine */
 #include <sys/types.h>
@@ -11,43 +12,48 @@ using namespace std;
 
 int globalVariable = 2;
 
-main()
+int main()
 {
+  string sIdentifier;
+  int    iStackVariable = 20;
 
-   string sIdentifier;
-   int    iStackVariable = 20;
+  pid_t   pID = fork();
+  pid_t   pID2 = 9;
 
-   pid_t pID = fork();
-   pid_t pID2 = 9;
-   if (pID == 0)                // child
-   {
-      // Code only executed by child process
+  if (pID == 0)                // child
+  {
+    // Code only executed by child process
 
-      pID2 = fork();
-      if (pID2 == 0)
-        fork();
+    pID2 = fork();
+    if (pID2 == 0) {
+      cout << "third child\n";
+      
+    } 
 
-      sIdentifier = "Child Process: ";
-      globalVariable++;
-      iStackVariable++;
-    }
-    else if (pID < 0)            // failed to fork
-    {
-        cerr << "Failed to fork" << endl;
-        exit(1);
-        // Throw exception
-    }
-    else                                   // parent
-    {
-      // Code only executed by parent process
-      sIdentifier = "Parent Process:";
-    }
-
-    // Code executed by both parent and child.
+    sIdentifier = "Child Process: ";
+    globalVariable++;
+    iStackVariable++;
   
-    cout << sIdentifier;
-    cout << " Global variable: " << globalVariable;
-    cout << " Stack variable: "  << iStackVariable;
-    cout << " pID2 " << pID << " " << pID2 << endl;
-}
-                
+  }
+  else if (pID < 0)            // failed to fork
+  {
+    cerr << "Failed to fork" << endl;
+    exit(1);
+    // Throw exception
+  }
+  else                                   // parent
+  {
+    // Code only executed by parent process
+    sIdentifier = "Parent Process:";
+    sleep(3);
+  }
+
+  // Code executed by both parent and child.
+
+  cout << sIdentifier;
+  cout << " Global variable: " << globalVariable;
+  cout << " Stack variable: "  << iStackVariable;
+  cout << " pID2 " << pID << " " << pID2 << endl;
+  exit(0);
+  }
+              
