8cc376cde9a7319b667ed24d522d9ec0dd265e71
Simon Strom <simon.strom@gmail.com>
2013-05-10 18:22:51 +0200

    indentations

--- lab3/malloc.c
+++ lab3/malloc.c
@@ -112,18 +112,18 @@ void * malloc(size_t nbytes)
   for(p= prevp->s.ptr;  ; prevp = p, p = p->s.ptr) {
     if(p->s.size >= nunits) {                           /* big enough */
       if (p->s.size == nunits)                          /* exactly */
-	prevp->s.ptr = p->s.ptr;
+	      prevp->s.ptr = p->s.ptr;
       else {                                            /* allocate tail end */
-	p->s.size -= nunits;
-	p += p->s.size;
-	p->s.size = nunits;
+      	p->s.size -= nunits;
+      	p += p->s.size;
+      	p->s.size = nunits;
       }
       freep = prevp;
       return (void *)(p+1);
     }
     if(p == freep)                                      /* wrapped around free list */
       if((p = morecore(nunits)) == NULL)
-	return NULL;                                    /* none left */
+	  return NULL;                                        /* none left */
   }
 }
 
