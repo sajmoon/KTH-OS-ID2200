6935762d4f3ced1ee06a8336ef908c4b20731402
Mattis Kancans Envall <mattiskan@gmail.com>
2015-11-10 15:08:09 +0100

    made shell exec command

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,20 +1,24 @@
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /* prompt -> prints to term. waits for input. */
-void prompt(char* input, int input_length)
+void prompt(char* input, const int input_length)
 {
   printf("$ ");
   fgets(input, input_length, stdin);
 
+  strtok(input, "\n"); /* does not strip on string with only \n in it. */
 }
 
 int main(int argc, char **argv, char **envp)
 {
   char input[70];
-  size_t input_length = 70;
+  const size_t input_length = 70;
 
   prompt(input, input_length);
 
-  printf("%s", input);
+  execvp(input, argv);
+  printf("Error: execwp failed\n");
   return 0;
 }
