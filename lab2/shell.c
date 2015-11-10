30c155998200ecec74c43fa1829bc784f809ac1b
Mattis Kancans Envall <mattiskan@gmail.com>
2015-11-10 14:39:14 +0100

    changed return type

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /* prompt -> prints to term. waits for input. */
-char* prompt(char* input, size_t input_length)
+void prompt(char* input, size_t input_length)
 {
   printf("$ ");
   getline(&input, &input_length, stdin);
-  return 0;
 }
 
 int main(int argc, char **argv, char **envp)
