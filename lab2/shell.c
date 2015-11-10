d81c69ced0359112ea10c13e8df56e0fdd7e013e
Simon Strom <simon.strom@gmail.com>
2015-11-10 14:33:00 +0100

    add prompt

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+
+/* prompt -> prints to term. waits for input. */
+char* prompt(char* input, size_t input_length)
+{
+  printf("$ ");
+  getline(&input, &input_length, stdin);
+  return 0;
+}
+
 int main(int argc, char **argv, char **envp)
 {
+  char input[70];
+  size_t input_length = 70;
+
+  prompt(input, input_length);
+
+  printf("%s", input);
+  return 0;
 }
