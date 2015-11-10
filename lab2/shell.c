4ac02f7ac3a47c81ef71379bddf822c5da5da05c
Mattis Kancans Envall <mattiskan@gmail.com>
2015-11-10 17:28:04 +0100

    added tokenization of args

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
@@ -34,19 +35,61 @@ void execute(char* command, char **argv)
   }
 }
 
+
+char** getargs(char* input) {
+  int i;
+  char * tokenc;
+  char *token;
+  char** args;
+
+  args = malloc(6*sizeof(char*));
+  
+  token = strtok(input, " ");
+
+  for(i =0; i<6; i++)
+  {
+    if(token != NULL){
+      tokenc = malloc(71*sizeof(char));
+      strcpy(tokenc, token);
+      args[i] = tokenc;
+      token = strtok(NULL, " ");
+    } else {
+      args[i] = NULL;
+    }
+  }
+
+  return args;
+}
+
+void freeargs(char** args) {
+  int i;
+  printf("free willy, dead or alive!\n");
+
+  for(i=0; i<6; i++) {
+    free(args[i]);
+  }
+}
+
 int main(int argc, char **argv, char **envp)
 {
-  char input[70];
-  const size_t input_length = 70;
+  char **args;
+  char input[71];
+  const size_t input_length = 71;
 
   while (1) {
     prompt(input, input_length);
+    args = getargs(input);
+
+    printf("args %s %s\n", args[0], args[1]);
 
     if(strcmp(input, "exit") == 0)
       break;
 
-    execute(input, argv);
+    execute(args[0], args);
+
+    freeargs(args);
   }
 
+  free(args);
   return 0;
 }
